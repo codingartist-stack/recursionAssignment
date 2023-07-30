c6e10276cbf8644f4413b3c5745e35a1e9706fc5
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-07-30 00:12:33 -0600

    rewrote the for loop for the Fibonacci sequence and the recursion loop. will have to move on to merge sort next

--- index.js
+++ index.js
@@ -3,8 +3,36 @@
 function fibSeq(number) {
   let a = 0;
   let b = 1;
+  let sequence = [a, b];
+
+  if (number === 0) {
+    sequence = [a];
+  }
+  if (number === 1) {
+    sequence = [a, b];
+  }
+
+  for (let i = 0; i < number - 1; i++) {
+    let c = a + b;
+
+    sequence.push(c);
+
+    a = b;
+    b = c;
+  }
+  return sequence;
 }
 
+console.log(fibSeq(8));
+
 // fib recursion
+
+function fibRecursion(fibNumber) {
+  if (fibNumber < 2) return fibNumber;
+
+  return fibRecursion(fibNumber - 1) + fibRecursion(fibNumber - 2);
+}
+
+console.log(fibRecursion(1));
 
 //merge sort
